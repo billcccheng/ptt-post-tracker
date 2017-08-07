2b4f6cab1a4fc1165e9c048f6305faf081ce17b2
Bill <iamkg05@gmail.com>
2017-08-06 23:31:52 -0500

    something

--- popup.js
+++ popup.js
@@ -4,41 +4,44 @@ function runPlugin() {
   chrome.tabs.executeScript(null, {
     file: "getPagesSource.js"
     }, function() {
-    if (chrome.runtime.lastError) {
-     alert('There was an error injecting script : \n' + chrome.runtime.lastError.message);
-    }
-  });
+      if (chrome.runtime.lastError) {
+        alert('There was an error injecting script : \n' + chrome.runtime.lastError.message);
+      }
+    });
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.action == "getSource") {
     let link = request.source;
     console.log(link);
-    saveChanges(link);
-    //getLinks("studyabroad");
+    let board = getBoardName(link);
+    saveBoard(board, link);
   }
 });
 
-function saveChanges(link) {
+function saveBoard(board, link){
+  chrome.storage.sync.get([board], function(result) {
+    let links = !!result ? [link] : result;
+    links = links.includes(link) ? links : links.unshift(link);
+    chrome.storage.sync.set({ board: links }, function() {
+      console.log("Link Saved");
+      getLinks(board);
+    });
+  });
+}
+
+function getBoardName(link) {
   if (!link) {
     return;
   }
   let subLink = link.substring(8); 
   let board = subLink.split("/")[2];
-
-  chrome.storage.sync.get(board, function(result) {
-    let links = !!result ? [link] : result;
-    links = links.includes(link) ? links : links.unshift(link);
-    chrome.storage.sync.set({ board: links }, function() {
-      console.log("Link Saved");
-    });
-  });
+  return board;
 }
 
 function getLinks(board) {
-  chrome.storage.sync.get(board, function(result) {
+  chrome.storage.sync.get([board], function(result) {
     console.log(result);
-    return result;
-  })
+  });
 }
 window.onload = runPlugin();
