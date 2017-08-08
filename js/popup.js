60d5fc9013df909e2c18ae2440f49e576e4eeb09
Cheng, Bill <ccheng@hq.corp.viasat.com>
2017-08-08 16:51:48 -0500

    css fix

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
 function runPlugin() {
   chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
+    file: "js/getPagesSource.js"
     }, function() {
       if (chrome.runtime.lastError) {
         alert('There was an error injecting script : \n' + chrome.runtime.lastError.message);
